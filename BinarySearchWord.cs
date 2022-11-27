35d007f3d41fe6797fea40565b90e8547cbd616b
RahulAid <raccess97@gmail.com>
2022-11-27 21:40:47 +0530

    Binary Search a Word from List Of Words

---
+++ BinarySearchWord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+public class BinarySearchWord
+{
+    public string FilePath = @"D:\BridgeLabzz\Algorithm-Programs\WordFile.json";
+
+    public void WordExistsOrNot()
+    {
+        string words = "OOPS,Array,Dictionary,Generics,Delegates,Linked List,Stack,Queue,Hash Table,Events,Regular Expressions";
+        File.WriteAllText(FilePath, JsonConvert.SerializeObject(words));
+
+        string wordData = File.ReadAllText(FilePath);
+        string word = JsonConvert.DeserializeObject<string>(wordData);
+        List<string> wordList = word.Split(',').ToList<string>();
+        wordList.Sort();
+
+        Console.WriteLine("Enter a Word to Search: ");
+        string wordToSearch = Console.ReadLine();
+
+        int index = wordList.BinarySearch(wordToSearch);
+
+        if (index < 0)
+        {
+            Console.WriteLine($"{wordToSearch} does not exist");
+        }
+        else
+        {
+            Console.WriteLine($"{wordToSearch} exists");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,25 +6,8 @@
         {
             Console.WriteLine("Welcome to Algorithm Programs");
 
-            RegexMessage obj = new RegexMessage();
-
-            Console.Write("\nEnter First Name: ");
-            string firstName = Console.ReadLine();
-            obj.FirstName(firstName);
-
-            Console.Write("\nEnter Full Name: ");
-            string fullName = Console.ReadLine();
-            obj.FullName(fullName);
-
-            Console.Write("\nEnter Mobile Number (Example: +(country code)-(10 digit number)): ");
-            string phoneNumber = Console.ReadLine();
-            obj.PhoneNumber(phoneNumber);
-
-            Console.Write("\nEnter Date (dd/mm/yyyy): ");
-            string date = Console.ReadLine();
-            obj.Date(date);
-
-            Console.WriteLine($"\nHello {obj.FirstName(firstName)}, \nWe have your full name as {obj.FullName(fullName)} in our system. \nYour contact number is {obj.PhoneNumber(phoneNumber)}. \nPlease, let us know in case of any clarification \nThank you \nBridgeLabz \n{obj.Date(date)}");
+            BinarySearchWord obj = new BinarySearchWord();
+            obj.WordExistsOrNot();
 
         }
     }
