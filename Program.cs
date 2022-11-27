144d1c8bbfa05d1902a204994edd16858377a07e
RahulAid <raccess97@gmail.com>
2022-11-27 15:52:16 +0530

    Permutations of a String

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Algorithm_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algorithm Programs");
+
+            Console.WriteLine("Enter a string value : ");
+            string word = Console.ReadLine();
+            int n = word.Length;
+            string answer = " ";
+
+            Console.WriteLine("\nAll possible strings are : ");
+
+            Console.WriteLine("\nIterative Method: ");
+            PermutationOfString.Iterative(word, answer);
+
+            Console.WriteLine("\nRecursive Method: ");
+            PermutationOfString.Recursion(word, 0, n - 1);
+        }
+    }
+}
