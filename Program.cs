664f4c74342bdd6113b8aca19b422f28fabe666c
RahulAid <raccess97@gmail.com>
2022-11-27 18:30:07 +0530

    Inserion Sort

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Algorithm_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs");
+
+            string[] names = { "OOPS", "Array", "Dictionary", "Generics", "Delegates", "LinkedList", "Stack", "Queue", "HashTable", "Events", "Regular Expressions" };
+            InsertionSortMethod objInsertion = new InsertionSortMethod();
+
+            Console.WriteLine("\nBefore sorting array elements are: ");
+            objInsertion.Display(names);
+
+            objInsertion.Sort(names);
+            Console.WriteLine("\n\nAfter sorting array elements are: ");
+            objInsertion.Display(names);
+
+            Console.ReadLine();
+        }
+    }
+}
