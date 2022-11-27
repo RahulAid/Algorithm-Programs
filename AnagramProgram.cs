767795ba25b451531292370dbc2ff7263c062288
RahulAid <raccess97@gmail.com>
2022-11-27 18:53:02 +0530

    Anagram Detection Program

---
+++ AnagramProgram.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+public class AnagramProgram
+{
+    public void AnagramDetectionProgram()
+    {
+        Console.WriteLine("\nEnter 1st string: ");
+        string firstString = Console.ReadLine();
+        Console.WriteLine("\nEnter 2nd string: ");
+        string secondString = Console.ReadLine();
+        char[] character1 = firstString.ToLower().ToCharArray();
+        char[] character2 = secondString.ToLower().ToCharArray();
+        Array.Sort(character1);
+        Array.Sort(character2);
+        string val1 = new string(character1);
+        string val2 = new string(character2);
+
+        if (val1 == val2)
+        {
+            Console.WriteLine("\nBoth strings are anagram");
+        }
+        else
+        {
+            Console.WriteLine("\nNot an anagram");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,17 +6,8 @@
         {
             Console.WriteLine("Welcome to Algorithm Programs");
 
-            int[] array = { 10, 20, 30, 40, 50, 60, 70 };
-            
-            MergeSortMethod objSort = new MergeSortMethod();
-
-            Console.WriteLine("\nArray elements Unsorted Order : ");
-            objSort.Display(array);
-
-            objSort.Sort(array, 0, array.Length - 1);
-            Console.WriteLine("\n\nArray elements Sorted Order : ");
-            objSort.Display(array);
-
+            AnagramProgram obj = new AnagramProgram();
+            obj.AnagramDetectionProgram();
             Console.ReadLine();
         }
     }
