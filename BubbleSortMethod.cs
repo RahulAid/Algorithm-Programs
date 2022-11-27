475d129e9ccbb485ce4ed1aabd6d8836d3e62425
RahulAid <raccess97@gmail.com>
2022-11-27 18:46:35 +0530

    Merge Sort

--- BubbleSortMethod.cs
+++ BubbleSortMethod.cs
@@ -5,22 +5,81 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-public class BubbleSortMethod
+public class MergeSortMethod
 {
-    public void Sort(int[] array)
+    // Function that Merges two subarrays of []array
+    // First subarray is array[left..mid]
+    // Second subarray is array[mid+1..right]
+    public void Merge(int[] array, int left, int mid, int right)
     {
-        int n = array.Length;
-        for (int i = 0; i < n - 1; i++)
+        // Find sizes of two subarrays to be merged
+        int sizeOfArray1 = mid - left + 1;
+        int sizeOfArray2 = right - mid;
+
+        // Create temp arrays
+        int[] tempLeft = new int[sizeOfArray1];
+        int[] tempRight = new int[sizeOfArray2];
+        int i, j;
+
+        // Copy data to temp arrays
+        for (i = 0; i < sizeOfArray1; ++i)
+            tempLeft[i] = array[left + i];
+        for (j = 0; j < sizeOfArray2; ++j)
+            tempRight[j] = array[mid + 1 + j];
+
+        // Merge the temp arrays
+        // Initial indexes of first and second subarrays
+        i = 0;
+        j = 0;
+
+        // Initial index of merged subarray array
+        int k = left;
+        while (i < sizeOfArray1 && j < sizeOfArray2)
         {
-            for (int index = 0; index < n - i - 1; index++)
+            if (tempLeft[i] <= tempRight[j])
             {
-                if (array[index] > array[index + 1])
-                {
-                    int temp = array[index];
-                    array[index] = array[index + 1];
-                    array[index + 1] = temp;
-                }
+                array[k] = tempLeft[i];
+                i++;
             }
+            else
+            {
+                array[k] = tempRight[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of tempLeft[] if any
+        while (i < sizeOfArray1)
+        {
+            array[k] = tempLeft[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of tempRight[] if any
+        while (j < sizeOfArray2)
+        {
+            array[k] = tempRight[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Function that Sorts arr[l..r] using Merge()
+    public void Sort(int[] array, int left, int right)
+    {
+        if (left < right)
+        {
+            // Find the middle point
+            int mid = left + (right - left) / 2;
+
+            // Sort first and second halves
+            Sort(array, left, mid);
+            Sort(array, mid + 1, right);
+
+            // Merge the sorted halves
+            Merge(array, left, mid, right);
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -7,13 +7,14 @@
             Console.WriteLine("Welcome to Algorithm Programs");
 
             int[] array = { 10, 20, 30, 40, 50, 60, 70 };
-            BubbleSortMethod objSort = new BubbleSortMethod();
+            
+            MergeSortMethod objSort = new MergeSortMethod();
 
-            Console.WriteLine("\nArray elements Unsorted order : ");
+            Console.WriteLine("\nArray elements Unsorted Order : ");
             objSort.Display(array);
 
-            objSort.Sort(array);
-            Console.WriteLine("\n\nArray elements Sorted order : ");
+            objSort.Sort(array, 0, array.Length - 1);
+            Console.WriteLine("\n\nArray elements Sorted Order : ");
             objSort.Display(array);
 
             Console.ReadLine();
